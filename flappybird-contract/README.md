66bca7bec1420ef297086da88a70cceea250de93
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-02-06 16:56:59 +0530

    deploy script
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- flappybird-contract/README.md
+++ flappybird-contract/README.md
@@ -1,1 +1,13 @@
 # Flappybird Contract
+
+
+
+```
+$> hardhat deploy
+
+Nothing to compile
+Wallet Ethereum Address: 0x6B4c696B623FA9A2A6D5b6E9101Ef19CD973bc3C
+Deploying Flappy Bird...
+Flappy Bird deployed to: 0xd3722f08E91261D70F88A7B9Df5B54ea8eC46194
+Done in 66.90s.
+```
--- flappybird-contract/deploy/00_deploy.js
+++ flappybird-contract/deploy/00_deploy.js
@@ -1,6 +1,7 @@
 require("hardhat-deploy")
 require("hardhat-deploy-ethers")
 
+const { ethers } = require("hardhat")
 const { networkConfig } = require("../helper-hardhat-config")
 
 
@@ -10,26 +11,14 @@ const wallet = new ethers.Wallet(private_key, ethers.provider)
 module.exports = async ({ deployments }) => {
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
-    //deploy Simplecoin
-    const SimpleCoin = await ethers.getContractFactory('SimpleCoin', wallet);
-    console.log('Deploying Simplecoin...');
-    const simpleCoin = await SimpleCoin.deploy(tokensToBeMinted);
-    await simpleCoin.deployed()
-    console.log('SimpleCoin deployed to:', simpleCoin.address);
 
-    //deploy FilecoinMarketConsumer
-    const FilecoinMarketConsumer = await ethers.getContractFactory('FilecoinMarketConsumer', wallet);
-    console.log('Deploying FilecoinMarketConsumer...');
-    const filecoinMarketConsumer = await FilecoinMarketConsumer.deploy();
-    await filecoinMarketConsumer.deployed()
-    console.log('FilecoinMarketConsumer deployed to:', filecoinMarketConsumer.address);
+    const FlappybirdStakeContract = await ethers.getContractFactory('FlappybirdStake', wallet);
+    console.log('Deploying Flappy Bird...');
+    const flappybirdStakeContract = await FlappybirdStakeContract.deploy();
+    const initialBalance = ethers.utils.parseEther("1000000000000000000")
 
-    //deploy DealRewarder
-    const DealRewarder = await ethers.getContractFactory('DealRewarder', wallet);
-    console.log('Deploying DealRewarder...');
-    const dealRewarder = await DealRewarder.deploy();
-    await dealRewarder.deployed()
-    console.log('DealRewarder deployed to:', dealRewarder.address);
+    await flappybirdStakeContract.deployed({ value: initialBalance })
+    console.log('Flappy Bird deployed to:', flappybirdStakeContract.address);
+
 }
